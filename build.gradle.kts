4bda9fb895244b57da8a4307596fb7fd43f317d5
ivanturkin <ivanturkinrussia@gmail.com>
2024-03-21 22:58:28 +0300

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.1.1")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    implementation("io.netty:netty-resolver-dns-native-macos:4.1.72.Final:osx-aarch_64")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}

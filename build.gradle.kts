9e218e3c3f83133e2a6d35475668c42243f54659
ivanturkin <ivanturkinrussia@gmail.com>
2024-03-21 23:31:54 +0300

    eureka client example

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,17 @@ repositories {
     mavenCentral()
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2023.0.0")
+    }
+}
+
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway:4.1.1")
+    // EUREKA CLIENT
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     implementation("io.netty:netty-resolver-dns-native-macos:4.1.72.Final:osx-aarch_64")
